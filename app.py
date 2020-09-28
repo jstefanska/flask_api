11aa99ad31ea027b341d94f0e2915a6978af35f0
jstefanska <44728213+jstefanska@users.noreply.github.com>
2020-09-28 12:26:05 +0200

    Change error messages for DB issues

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import json
 import psycopg2
 from psycopg2 import sql
 import os
-from datetime import datetime
 
 app = Flask(__name__)
 
@@ -20,7 +19,9 @@ def add_hashtag():
             if 'DB_PASS' not in os.environ:
                 abort(make_response(jsonify(message="'DB_PASS' environment variable does not exist"), 400))
             else:
-                db_pass = os.getenv('DB_PASS')
+                pass
+
+            db_pass = os.getenv('DB_PASS')
 
             conn = psycopg2.connect(host='localhost', port=5432, dbname='postgres', user='postgres',
                                     password=db_pass)
@@ -37,7 +38,7 @@ def add_hashtag():
         except psycopg2.OperationalError:
             abort(make_response(jsonify(message="Unable to connect to the database!"), 502))
 
-        except psycopg2.errors.SyntaxError:
+        except psycopg2.errors.UniqueViolation:
             abort(make_response(jsonify(message="Hashtag already exists in database."), 409))
 
 
@@ -47,18 +48,22 @@ def get_hashtag(hashtag):
         if 'DB_PASS' not in os.environ:
             abort(make_response(jsonify(message="'DB_PASS' environment variable does not exist"), 400))
         else:
-            db_pass = os.getenv('DB_PASS')
+            pass
+
+        db_pass = os.getenv('DB_PASS')
 
         conn = psycopg2.connect(host='localhost', port=5432, dbname='postgres', user='postgres',
                                 password=db_pass)
         conn.autocommit = True
         cur = conn.cursor()
 
-        query = sql.SQL("SELECT * FROM public.twitter WHERE hashtag = %s ORDER BY tweet_id DESC LIMIT 10").format()
+        query = sql.SQL("SELECT * FROM public.twitter WHERE hashtag = %s ORDER BY id DESC LIMIT 10").format()
         cur.execute(query, (hashtag, ))
         tweets_columns = cur.fetchall()
+        cur.close()
+        conn.close()
 
-        tweets_data = [{"tweet_id": column[1], "tweet_content": column[0]} for column in tweets_columns]
+        tweets_data = [{"tweet_id": column[1], "tweet_content": column[2]} for column in tweets_columns]
 
         if not tweets_data:
             abort(make_response(jsonify(message="Database does not contain tweets with this hashtag."), 400))
