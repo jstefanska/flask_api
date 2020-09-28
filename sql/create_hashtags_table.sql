a8501b2a44e88d61c7c16400f86e46c1c876fb7d
jstefanska <44728213+jstefanska@users.noreply.github.com>
2020-09-28 12:35:47 +0200

    Add SQL scripts to create tables in database.

---
+++ sql/create_hashtags_table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE public.hashtags (
+	id serial NOT NULL,
+	hashtag varchar NOT NULL,
+	CONSTRAINT hashtag_pk PRIMARY KEY (hashtag)
+);
---
+++ sql/create_twitter_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE public.twitter (
+	id serial NOT NULL,
+	tweet_id varchar NULL,
+	tweet_content varchar NULL,
+	hashtag varchar NULL,
+	datetime varchar NULL
+);
